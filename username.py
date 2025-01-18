1818e71e6c3c4187d4a19301fc8eeed8272d18dd
Stark <ankit566kota@gmail.com>
2025-01-18 16:43:27 +0530

    updated

--- username.py
+++ username.py
@@ -1,83 +1,88 @@
 # pre defines
 adjs = [
-    "Brave", "Clever", "Witty", "Fierce", "Mighty", "Lively", "Swift", 
-    "Bold", "Silent", "Happy", "Curious", "Epic", "Shiny", "Zesty", 
-    "Lucky", "Rustic", "Frosty", "Cheerful", "Cosmic", "Vibrant",
-    "Noble", "Mystic", "Daring", "Ancient", "Glowing", "Ethereal", 
-    "Enchanted", "Legendary", "Sacred", "Magical", "Luminous", 
-    "Celestial", "Shadowy", "Divine", "Unseen", "Mythical", 
-    "Shimmering", "Golden", "Arcane", "Infinite",
-    "Stormy", "Icy", "Blazing", "Thorny", "Verdant", "Sunny", 
-    "Frozen", "Wavy", "Breezy", "Rainy", "Dusky", "Windy", 
-    "Foggy", "Wild", "Rocky", "Starry", "Crimson", "Azure", 
-    "Emerald", "Amber",
-    "Quantum", "Neon", "Robotic", "Synthetic", "Digital", 
-    "Galactic", "Chrome", "Viral", "Electric", "Virtual", 
-    "Dynamic", "Reactive", "Augmented", "Automated", "Coded", 
-    "Cybernetic", "Holographic", "Cosmic", "Energized", "Advanced",
-    "Snappy", "Zany", "Epic", "Vivid", "Playful", "Fancy", 
-    "Gentle", "Swift", "Mellow", "Bold", "Sharp", "Chilly", 
-    "Cozy", "Whimsical", "Perky", "Quirky", "Radiant", 
-    "Elegant", "Stellar", "Brilliant"
+    "Savage", "Ruthless", "Chill", "Deadly", "Fearless", "Epic", "Twisted", 
+    "Dark", "Icy", "Frosty", "Blazing", "Vicious", "Radical", "Funky", 
+    "Insane", "Silent", "Grim", "Reckless", "Wicked", "Shadowy",
+    "Infinite", "Legendary", "Mythic", "Daring", "Dynamic", "Cunning", 
+    "Electric", "Neon", "Phantom", "Rogue", "Venomous", "Glitched", 
+    "Quantum", "Atomic", "Lethal", "Toxic", "Hypnotic", "Sinister", 
+    "Cyber", "Doomed", "Infernal",
+    "Stealthy", "Elite", "Spiky", "Snazzy", "Flashy", "Fiery", "Glowing", 
+    "Sharp", "Lunar", "Solar", "Stormy", "Warped", "Eternal", "Blazing", 
+    "Cosmic", "Frozen", "Mighty", "Funky", "Edgy", "Loyal",
+    "Savvy", "Fierce", "Dapper", "Slick", "Raging", "Vivid", "Shiny", 
+    "Zesty", "Perky", "Chill", "Bold", "Cozy", "Killer", "Quirky", 
+    "Funky", "Stellar", "Dynamic", "Spooky", "Iron", "Golden"
 ]
 
+
 nouns = [
-    "Falcon", "Wolf", "Eagle", "Phoenix", "Panther", "Bear", "Tiger", "Fox", 
-    "Raven", "Owl", "Comet", "Star", "Storm", "Glacier", "River", "Shadow", 
-    "Thorn", "Forest", "Blizzard", "Flame",
-    "Wizard", "Knight", "Dragon", "Titan", "Elf", "Goblin", "Valkyrie", 
-    "Unicorn", "Warden", "Sorcerer", "Oracle", "Witch", "Bard", "Dagger", 
-    "Archer", "Wanderer", "Mystic", "Guardian", "Paladin", "Seeker",
-    "Circuit", "Atom", "Cypher", "Pixel", "Rocket", "Drone", "Satellite", 
-    "Nexus", "Matrix", "Quantum", "Bot", "Gear", "Laser", "Synth", "Ray", 
-    "Beacon", "Byte", "Code", "Reactor", "Galaxy",
-    "Hunter", "Warrior", "Ranger", "Sniper", "Swordsman", "Gladiator", 
-    "Crusader", "Raider", "Assassin", "Nomad", "Protector", "Chaser", 
-    "Fighter", "Scout", "Jumper", "Challenger", "Striker", "Survivor", 
-    "Champion", "Rebel",
-    "Pirate", "Captain", "Sailor", "Voyager", "Rider", "King", "Queen", 
-    "Monarch", "Hero", "Villain", "Gambler", "Scholar", "Architect", 
-    "Pioneer", "Visionary", "Nomad", "Pathfinder", "Creator", "Dreamer", 
-    "Explorer"
+    "Slayer", "Rogue", "Sniper", "Shadow", "Hunter", "Phantom", "Beast", 
+    "Dragon", "Titan", "Eagle", "Falcon", "Fox", "Wolf", "Raven", "Blaze", 
+    "Viper", "Comet", "Flare", "Storm", "Nebula",
+    "Glitch", "Knight", "Cyber", "Dagger", "Blizzard", "Circuit", 
+    "Drone", "Matrix", "Pixel", "Warden", "Nomad", "Valkyrie", 
+    "Witch", "Oracle", "Guardian", "Seeker", "Warrior", "Ranger", "Reaper", 
+    "Swordsman",
+    "Rocket", "Laser", "Synth", "Bot", "Gear", "Cypher", "Quantum", 
+    "Galaxy", "Bolt", "Beacon", "Code", "Striker", "Challenger", "Fighter", 
+    "Survivor", "Scout", "Raider", "Chaser", "Champion", "Explorer",
+    "Pirate", "Sailor", "Hero", "Villain", "Pathfinder", "Gambler", 
+    "Visionary", "Dreamer", "Creator", "Nomad", "Stalker", "Voyager", 
+    "Crusader", "Rebel", "Legend", "Thorn", "Reactor", "Spark", 
+    "Dominator", "Hacker"
 ]
+
 
 digs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 spl_chars = [
-    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
-    "=", "+", "{", "}", "[", "]", "|", "\\", ":", ";", "\"", "'", 
-    "<", ">", ",", ".", "?", "/", "~", "`"
+    "_", "-", ".", "~", "!", "@", "#", "$", "%", "&", "*", "+", 
+    "=", "|", "^", ">", "<"
 ]
+
 
 
 # random
 import random
+while True:
+    N=random.choice(nouns)
+    A=random.choice(adjs)
+    D=random.choice(digs)
+    S=random.choice(spl_chars)
 
-N=random.choice(nouns)
-A=random.choice(adjs)
-D=random.choice(digs)
-S=random.choice(spl_chars)
-
-inp=input("Y for Yes \nN for No \nDo you want to add preferences:")
-inp=inp.lower()
-if inp=="y":
-    UIN=input("Enter preference here: ")
-    U_IN=("_"+UIN+"_")
-    nme=[N,A,D,S,U_IN]
-elif inp=="n":
-    nme=[N,A,D,S]
-else:
-    print("Please enter Y or N.")
+    # Appending Preferences
+    while True:
+        inp=input("Y for Yes \nN for No \nDo you want to add preferences:").lower()
+        if inp=="y":
+            UIN=input("Enter preference here: ")
+            U_IN=("_"+UIN+"_")
+            nme=[N,A,D,S,U_IN]
+            break
+        elif inp=="n":
+            nme=[N,A,D,S]
+            break
+        else:
+            print("Invalid input. Please enter Y or N.")
 
 
-random.shuffle(nme)
-username="".join(nme)
+    # Randomizing the order in username
+    random.shuffle(nme)
+    username="".join(nme)
 
-f=open("names.txt","a")
-f.write(username+"\n")
-f.close
+    #File handling
+    f=open("names.txt","r")
+    existing_name=f.read().splitlines()
 
-f=open("names.txt","r")
-print(f.read())
+    if username in existing_name:
+        print(f"Username '{username}' already exists!\nGo again!")
+        continue
+    else:
+        f=open("names.txt","w")
+        f.write(username+"\n")
+        f.close
+        break
+# Printing the username
+print(f"Username Generated!\nUsername: {username}")
 
